b6c411b75023acb461437842af3b25b5665c9ae6
Alejo Herrera <alejoherrera1166@gmail.com>
2024-03-26 07:17:59 -0300

    Funcion para editar color agregada correctamente.

--- src/controllers/colores.controller.js
+++ src/controllers/colores.controller.js
@@ -1,44 +1,56 @@
 import Color from "../database/Models/Color.js";
 
-
-export const crearColor = async(req, res) => {
+export const crearColor = async (req, res) => {
   try {
-    console.log(req.body)
+    console.log(req.body);
     const colorNuevo = new Color(req.body);
     await colorNuevo.save();
     res.status(201).json({
-      mensaje: "El color fue creado correctamente"
-    })    
+      mensaje: "El color fue creado correctamente",
+    });
   } catch (error) {
-    console.log(error)
+    console.log(error);
     res.status(400).json({
-      mensaje:"El color no pudo ser dado de alta."
-    })
-    
+      mensaje: "El color no pudo ser dado de alta.",
+    });
   }
 };
 
-export const listarColores = async(req, res) => {
+export const listarColores = async (req, res) => {
   try {
     const colores = await Color.find();
     res.status(200).json(colores);
   } catch (error) {
-   console.error(error)
-   res.status(404).json({
-    mensaje: "No se pudo obtener la lista de colores."
-   })
+    console.error(error);
+    res.status(404).json({
+      mensaje: "No se pudo obtener la lista de colores.",
+    });
   }
 };
 
-export const obtenerColor = async(req,res) => {
+export const obtenerColor = async (req, res) => {
   try {
-    console.log(req.params.id)
-    const colorBuscado = await Color.findById(req.params.id)
+    console.log(req.params.id);
+    const colorBuscado = await Color.findById(req.params.id);
     res.status(200).json(colorBuscado);
   } catch (error) {
-    console.error(error)
+    console.error(error);
     res.status(404).json({
-      mensaje: "No se encontró el color buscado."
-    })
+      mensaje: "No se encontró el color buscado.",
+    });
   }
+};
+
+export const editarColor = async(req,res) => {
+  try {
+    const colorBuscado = await Color.findById(req.params.id);
+    if(!colorBuscado){
+      return res.status(404).json({Mensaje:"El color no fue encontrado."})
+    }
+    await Color.findByIdAndUpdate(req.params.id, req.body);
+    res.status(200).json({mensaje: "El color fue editado correctamente."})
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({mensaje:"Ocurrio un error al editar el color."})
   }
+}
--- src/routes/colores.routes.js
+++ src/routes/colores.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { crearColor, listarColores, obtenerColor } from "../controllers/colores.controller.js";
+import { crearColor, editarColor, listarColores, obtenerColor } from "../controllers/colores.controller.js";
 
 const router = Router();
 
 router.route("/colores").get(listarColores).post(crearColor);
-router.route("/colores/:id").get(obtenerColor);
+router.route("/colores/:id").get(obtenerColor).put(editarColor);
 
 export default router;
